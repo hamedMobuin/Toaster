96fada538827bfb0228326ce2e444077564b885c
h.karami <hamedzrmb@gmail.com>
2024-01-27 12:10:47 +0330

    init toaster

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ToastLibTest"
+include(":app")
+include(":toaster")
